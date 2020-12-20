03c37e5472a057f008147f240ac6fca560a11580
Seungwook Lee <seungwook1024@gmail.com>
2020-12-21 02:18:53 +0900

    Fix bug, cleanup code.

--- code/calculate_local_nash.py
+++ code/calculate_local_nash.py
@@ -11,8 +11,7 @@ def calculate_local_nash(player_i, player_j, A_i, A_j, g):
     room_to_improve1 = (player_i.valuation(A_i + g) * player_j.valuation(A_j - g) -
                         player_i.valuation(A_i) * player_j.valuation(A_j) > 0) and np.all(A_j - g >= 0)
 
-    room_to_improve2 = (player_i.valuation(g) > 0) and (
-        player_j.valuation(g) == 0)
+    room_to_improve2 = player_i.valuation(g) > 0 and player_j.valuation(g) == 0
 
     debug = False
     if debug:
--- code/run_allocation.py
+++ code/run_allocation.py
@@ -12,38 +12,41 @@ def main():
     Set number of players n and goods m
     '''
     debug = False
-    n = 3
+    # Players
+    n = 5
+    # Goods
+    m = 3*n
+    # Types of goods. Select (m or n)
+    items = n
+    # Sum of valuations
+    K = 500
 
-    m = 2*n
-    items = m
-    quantities = np.ones(items)
+    quantities = np.ones(items)*items/m
     goods = Goods(items, quantities)
+
+    # Create player instances
     players = []
     preference = np.ones(items)
-    K = 500
+
     for i in range(n):
         players.append(Player(i, goods.total_set, K))
         # players.append(Player(i, goods.total_set, K, preference))
         print("player "+str(i)+"'s preference: "+str(players[i].value_array))
-    # players.append(Player(0, goods.total_set, [1., 0.]))
-    # players.append(Player(1, goods.total_set, [0., 1.]))
 
     '''
     Setup initial allocation
     '''
-    A_all = np.ones(m)
+    A_all = quantities
+
     for i in range(n-1):
-        A_all = np.vstack([A_all, np.zeros(m)])
+        A_all = np.vstack([A_all, np.zeros(items)])
     A_init = copy.deepcopy(A_all)
     print("A_all:"+str(A_all))
-    # A_all = np.ones(n)*m/n
-    # for i in range(int(n/3.)):
-    #     A_all[i] += 1
-    #     A_all[len(A_all)-1-i] -= 1
     '''
     Search maximum valuation
     '''
     max_valuation = 0
+
     for i in range(n):
         max_valuation = max(max_valuation, players[i].valuation(A_all[i]))
 
@@ -52,40 +55,56 @@ def main():
     '''
     num_iter = 0
     iter_flag = True
+
     while iter_flag:
         iter_flag = False
-        for i in range(m):
-            g = np.zeros(m)
+
+        for i in range(len(A_all[0])):
+            g = np.zeros_like(A_all[0])
             g[i] = 1  # g \in A_j st [0,1]
+
             if debug:
-                print("===========")
-                print("g   = "+str(g))
+                print("=========== m="+str(i))
+                print("g = "+str(g))
             permute = permutations(players, 2)
+
             for player_i_j in permute:
                 if player_i_j[0].id == player_i_j[1].id:
                     continue
+
                 num_iter += 1
+
+                # Allocations for player i and j
                 A_i = A_all[player_i_j[0].id]
                 A_j = A_all[player_i_j[1].id]
                 if debug:
                     print("---")
                     print("Player [i,j] = " +
                           str([player_i_j[0].id, player_i_j[1].id]))
-                    print("iter: "+str(num_iter))
-                    print("A   = "+str(A_all))
-                    print("A_i = "+str(A_i))
+                    print("iter: " + str(num_iter))
+                    print("A   = " + str(A_all))
+                    print("A_i = " + str(A_i))
                     print("A_i+g = "+str(A_i+g))
-                    print("A_j = "+str(A_j))
+                    print("A_j = " + str(A_j))
                     print("A_j-g = "+str(A_j-g))
 
-                if calculate_local_nash(player_i_j[0], player_i_j[1], A_i, A_j, g):
+                if np.any(A_j - g < 0):  # Avoid impossible cases
+                    continue
+                elif calculate_local_nash(player_i_j[0], player_i_j[1], A_i, A_j, g):
+                    '''
+                    Transfer a good g if 
+                        vi(Ai+g)* vj (Aj -g) > vi(Ai)* vj (Aj ))
+                    -> True if allocations can be imnproved    
+                    '''
                     A_all[player_i_j[0].id] += g
                     A_all[player_i_j[1].id] -= g
                     if debug:
                         print("Allocation = "+str(A_all))
                     iter_flag = True
 
-    # num_iter *= 0.5
+    '''
+    Result summary
+    '''
     print("===========")
     print("Number of players: "+str(n))
     print("Number of goods  : "+str(m))
@@ -95,7 +114,7 @@ def main():
     print(str(A_all))
     print("Values: ")
     for i in range(n):
-        print("v("+str(i)+") = "+str(players[i].valuation(A_all[i])))
+        print("v_"+str(i)+"(A) = "+str(players[i].valuation(A_all[i])))
     print("Num of itereations = "+str(num_iter))
     print("Max valuation v(M) = "+str(max_valuation))
 
